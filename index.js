dc36c5759c31eca1c1fce79937f974a172b91bac
maharib820 <maharib820@gmail.com>
2026-06-07 05:40:33 -0700

    final one

--- index.js
+++ index.js
@@ -24,13 +24,17 @@ if (!admin.apps.length) {
 
 // ---------- Nodemailer Transporter ----------
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  // service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
   tls: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
+    ciphers: "SSLv3"
   }
 });
 
